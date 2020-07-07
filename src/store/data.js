1401c9961864a4775c00421a8c0c03dcffe26877
Efen Stakes <efenstakes101@gmail.com>
2020-07-07 13:20:11 +0300

    add dummy data for the app

---
+++ src/store/data.js
@@ -0,0 +1,118 @@
+
+
+
+
+import image_1 from '../assets/images/1.jpg'
+import image_2 from '../assets/images/2.jpg'
+import image_3 from '../assets/images/3.jpg'
+import image_4 from '../assets/images/4.jpg'
+import image_5 from '../assets/images/5.jpg'
+import image_6 from '../assets/images/6.jpg'
+import image_7 from '../assets/images/7.jpg'
+import image_8 from '../assets/images/8.jpg'
+
+
+
+
+const meals = [
+
+    {
+        id: '1',
+        name: 'Pilau Swahili',
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+        rating: 3.9,
+        price: 2.4,
+        image: image_1 // '../assets/images/1.jpg'
+    },
+    {
+        id: '2',
+        name: 'Swahili Hamburger',
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+        rating: 3.7,
+        price: 4,
+        image: image_2 // '../assets/images/1.jpg'
+    },
+    {
+        id: '3',
+        name: 'Pilau',
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+        rating: 4.1,
+        price: 6.5,
+        image: image_3 // '../assets/images/1.jpg'
+    },
+    {
+        id: '4',
+        name: 'Spanish Omelete',
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+        rating: 4.9,
+        price: 8.5,
+        image: image_4 // '../assets/images/1.jpg'
+    },
+    {
+        id: '5',
+        name: 'Zege',
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+        rating: 3.5,
+        price: 2.4,
+        image: image_5 // '../assets/images/1.jpg'
+    },
+    {
+        id: '6',
+        name: 'Beef Samosa',
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+        rating: 3.6,
+        price: 2.4,
+        image: image_6 // '../assets/images/1.jpg'
+    },
+    {
+        id: '7',
+        name: 'Agon Beer',
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+        rating: 4.4,
+        price: 3,
+        image: image_7 // '../assets/images/1.jpg'
+    },
+    {
+        id: '8',
+        name: 'Passion Smoothie',
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+        rating: 4.6,
+        price: 2.6,
+        image: image_8 // '../assets/images/1.jpg'
+    },
+    {
+        id: '9',
+        name: 'Fish Ugali',
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+        rating: 4.7,
+        price: 2.9,
+        image: image_6  // '../assets/images/1.jpg'
+    },
+    {
+        id: '10',
+        name: 'Mango Smoothie',
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+        rating: 4.7,
+        price: 2.8,
+        image: image_7  // '../assets/images/1.jpg'
+    },
+    {
+        id: '11',
+        name: 'Little Island Laqua',
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+        rating: 4.7,
+        price: 2.8,
+        image: image_3  // '../assets/images/1.jpg'
+    },
+    {
+        id: '12',
+        name: 'Big Boy Laqua',
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+        rating: 4.8,
+        price: 2.8,
+        image: image_8  // '../assets/images/1.jpg'
+    },
+
+]
+
+export default meals
